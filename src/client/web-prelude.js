ca924db3a49a3d35ce97484757ee1751bec8639e
Joseph Gentle <josephg@gmail.com>
2013-04-26 14:39:49 -0700

    Added missing web prelude

---
+++ src/client/web-prelude.js
@@ -0,0 +1,5 @@
+// This file is included at the top of the compiled client JS.
+
+// All the modules will just add stuff to exports, and it'll all get exported.
+var exports = window.sharejs = {version: '0.7.0'};
+
